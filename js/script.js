fab3e59aeb1122fd3ec2aa64a1d3ad4f04a26209
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-13 22:29:22 +0530

    Looping Generators!

--- js/script.js
+++ js/script.js
@@ -4,28 +4,17 @@
 
 /* eslint-disable */
 
-function ajax(url) {
-  fetch(url)
-    .then(data => data.json())
-    .then(data => dataGen.next(data));
+function* lyrics() {
+  yield `But don't tell my heart`;
+  yield `My achy breaky heart`;
+  yield `I just don't think he'd understand`;
+  yield `And if you tell my heart`;
+  yield `My achy breaky heart`;
+  yield `He might blow up and kill this man`;
 }
 
-function* steps() {
-  console.log('Fetching harry');
-  const harry = yield ajax('https://api.github.com/users/IamManchanda');
-  console.log(harry);
-  console.log('\n');
+const achy = lyrics();
 
-  console.log('Fetching wes');
-  const wes = yield ajax('https://api.github.com/users/wesbos');
-  console.log(wes);
-  console.log('\n');
-
-  console.log('Fetching fatJoe');
-  const fatJoe = yield ajax('https://api.discogs.com/artists/51988');
-  console.log(fatJoe);
-  console.log('\n');
+for (const line of achy) {
+  console.log(line)
 }
-
-const dataGen = steps();
-dataGen.next();
